995dbc096c10403b7c501b5b7ca2653e4a65be9c
namchylartysh <artyshnam@gmail.com>
2023-06-22 22:56:06 +0600

    added

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         boolean flag = true;
         System.out.println("Hello world");
-        System.out.println("Hello from Andrew");
+        System.out.println("Hello from Artysh");
         System.out.println("Hello again");
         while(flag != false) {
             System.out.println("I'm alive!");
